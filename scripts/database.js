7226a366e53a0fa127e4e7bc13b29249bf42097a
Kristen Carroll <carroll.kristen@gmail.com>
2022-01-29 12:01:39 -0600

    added commas

--- scripts/database.js
+++ scripts/database.js
@@ -6,21 +6,21 @@ const database = {
             size: 3,
             species: "sheepshead",
             location: "Atlantic Coast"
-        }
+        },
         {
             name: "Brando",
             food: "kelp",
             size: 12,
             species: "Longfin Tetra",
             location: "Gulf of Mexico"
-        }
+        },
         {
             name: "Madame President",
             food: "sand dollars",
             size: 8,
             species: "Electric Blue Acara Cichlid",
             location: "Great Blue Hole"
-        }
+        },
         {
             name: "Pip and Lily",
             food: "fish flakes",

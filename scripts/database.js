95588f5faaee7b79d4145b51fe41b3b5661f4443
Kristen Carroll <carroll.kristen@gmail.com>
2022-02-12 17:39:35 -0600

    revisiting innerhtml problems

--- scripts/database.js
+++ scripts/database.js
@@ -3,28 +3,28 @@ const database = {
         {
             name: "Bart",
             food: "crustaceans",
-            size: 3,
+            length: 3,
             species: "sheepshead",
             location: "Atlantic Coast"
         },
         {
             name: "Brando",
             food: "kelp",
-            size: 12,
+            length: 12,
             species: "Longfin Tetra",
             location: "Gulf of Mexico"
         },
         {
             name: "Madame President",
             food: "sand dollars",
-            size: 8,
+            length: 8,
             species: "Electric Blue Acara Cichlid",
             location: "Great Blue Hole"
         },
         {
             name: "Pip and Lily",
             food: "fish flakes",
-            size: 4,
+            length: 4,
             species: "Ryukin Goldfish",
             location: "Pet Smart"
         }
